8c2d795237645be4465dfad76b7cc544506dd68b
Luk3210 <155194413+Luk3210@users.noreply.github.com>
2024-01-06 18:10:16 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,8 @@
+//intitial install
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+  if (reason === "install") {
+    chrome.tabs.create({
+      url: "onboarding.html",
+    });
+  }
+});
